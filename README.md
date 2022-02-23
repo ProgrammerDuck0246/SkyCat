e3e667a009c59211c0f4746128dd8db1917fe33c
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2022-02-23 10:26:28 +0700

    fix(project): fix project structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# SkyCat Bot
+
+## Contributing
+
+This project used commitizen for proper commit messages. Please use `npm run commit` instead of git commit for your pull requests to be accepted.
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: 'always',
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
+
+module.exports = options
